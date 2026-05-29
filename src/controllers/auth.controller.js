076ae6d414f560a76e800f1d0e1201f0ed4bcb1b
Dipanshu Garg <your-email@example.com>
2026-05-29 20:18:52 +0530

    Implement user authentication with JWT and MongoDB

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,102 @@
+const userModel = require("../models/user.model");
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+/**
+ * @name registerUserController
+ * @desc Register a new user expected to receive username, email and password in the request body
+ * @access Public
+ */
+async function registerUserController(req,res){
+    const {username,email,password}= req.body
+
+    if(!username || !email || !password){
+        res.status(400).json({
+            message:'Username, email and password are required'
+        })
+    }
+
+    const isUserExist= await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(isUserExist){
+        res.status(400).json({
+            message:'Username or email already exists'
+        })
+    }
+
+    const hash = await bcrypt.hash(password,10)
+
+    const user= await userModel.create({
+        username,
+        email,
+        password:hash
+    })
+
+    const token = jwt.sign(
+        {id:user._id, username:user.username},
+        process.env.JWT_SECRET,
+        {expiresIn:'1d'}
+    )
+
+    res.cookie('token',token)
+
+    res.status(201).json({
+        message:'User registered successfully',
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
+    })
+}
+
+/**
+ * @name loginUserController
+ * @desc Login a user expected to receive email and password in the request body
+ * @access Public
+ */
+async function loginUserController(req,res){
+    const {email,password}= req.body
+
+    if(!email || !password){
+        res.status(400).json({
+            message:'Email and password are required'
+        })
+    }
+    const user= await userModel.findOne({email})
+    if(!user){
+        res.status(400).json({
+            message:'Invalid email or password'
+        })
+    }
+    const isPasswordValid = await bcrypt.compare(password,user.password)
+    if(!isPasswordValid){
+        res.status(400).json({
+            message:'Invalid email or password'
+        })
+    }
+    const token = jwt.sign(
+        {id:user._id, username:user.username},
+        process.env.JWT_SECRET,
+        {expiresIn:'1d'}
+    )
+    res.cookie('token',token)
+
+    res.status(200).json({
+        message:'User logged in successfully',
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
+    })
+}
+
+module.exports= {
+    registerUserController,
+    loginUserController
+}
