f7317c3c40e7f7bc7f963d0dd78d7982bcfef100
Dipanshu Garg <your-email@example.com>
2026-05-31 16:21:02 +0530

    Build authentication UI and shared styling system

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,23 +1,55 @@
-import './auth.form.scss'
+import "./auth.form.scss";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <main>
-        <div className="form-container">
-            <h1>Login</h1>
-            <form>
-                <div className="input-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Enter your email" />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" placeholder="Enter your password" />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    </main>
-  )
-}
+      <div className="form-container">
+        <h1>Login</h1>
+        <p className="subtitle">Welcome back! Sign in to continue.</p>
 
-export default Login
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+            />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <button type="submit" className="button primary-button submit-btn">
+            Login
+          </button>
+        </form>
+
+        <div className="auth-footer">
+          <span>Don't have an account?</span>
+
+          <span className="auth-link" onClick={() => navigate("/register")}>
+            Register
+          </span>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,11 +1,59 @@
-
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+  };
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
 
-export default Register
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Enter your username"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button type="submit" className="button primary-button">
+            Register
+          </button>
+        </form>
+
+        <div className="auth-footer">
+          <span>Already have an account?</span>
+
+          <a className="auth-link" onClick={() => navigate("/login")}>
+            Login
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
