3ad10dae6be54b37ebb1ca6f089e6a4b2bdf2576
Dipanshu Garg <your-email@example.com>
2026-06-08 21:41:13 +0530

    fix: allow localhost and frontend URL in cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,22 +6,10 @@ const app= express()
 
 app.use(express.json())
 app.use(cookieParser())
-const allowedOrigins = [
-  "http://localhost:5173",
-  process.env.FRONTEND_URL,
-];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  })
-);
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 /*required routes*/
 const authRouter = require('./routes/auth.routes')
